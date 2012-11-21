e611ef44fb4bc2532ac2c8c0847aa6c0a1d98889
yongkangliu <yongkangliu@gmail.com>
2012-11-21 16:02:11 -0500

    ok

--- KDD/src/kdd/data/DiscernableData.java
+++ KDD/src/kdd/data/DiscernableData.java
@@ -1,5 +1,6 @@
 package kdd.data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +9,8 @@ import java.util.Map;
 public class DiscernableData {
     private static DiscernableData instance = null;
 
+    private String[] desiredSet;
+    private List<String>[][] desiredAlphaReducts;
     private Map<String, Integer> desiredMap = new HashMap<String, Integer>();
     private Map<String, Integer> unDesiredMap = new HashMap<String, Integer>();
 
@@ -20,11 +23,20 @@ public class DiscernableData {
     private DiscernableData(String[] desiredSet, String[] undesiredSet, List<String>[][] dataSet) {
         this.dataSet = dataSet;
 
-        for (int i = 0; i < desiredSet.length; i++) {
-            this.desiredMap.put(desiredSet[i], i);
+        this.desiredSet = desiredSet;
+        if (desiredSet != null) {
+            this.desiredAlphaReducts = new List[desiredSet.length][];
         }
-        for (int i = 0; i < undesiredSet.length; i++) {
-            this.unDesiredMap.put(undesiredSet[i], i);
+
+        if (desiredSet != null) {
+            for (int i = 0; i < desiredSet.length; i++) {
+                this.desiredMap.put(desiredSet[i], i);
+            }
+        }
+        if (undesiredSet != null) {
+            for (int i = 0; i < undesiredSet.length; i++) {
+                this.unDesiredMap.put(undesiredSet[i], i);
+            }
         }
     }
 
@@ -45,8 +57,95 @@ public class DiscernableData {
     }
 
     public ReductData calculateReductData() {
-        // TODO
-        return ReductData.initialize(null);
+        for (int i = 0; i < this.desiredAlphaReducts.length; i++) {
+            List<String>[] inputList = new List[this.dataSet.length];
+            for (int j = 0; j < this.dataSet.length; j++) {
+                if (this.dataSet[j][i].get(0) == null) {
+                    this.desiredAlphaReducts[i] = null;
+                    break;
+                } else {
+                    inputList[j] = this.dataSet[j][i];
+                }
+            }
+            this.desiredAlphaReducts[i] = convertCNF2DNF(absorptionLaw(inputList));
+        }
+
+        List<List<String>> reductsList = new ArrayList<List<String>>();
+        for (int i = 0; i < this.desiredAlphaReducts.length; i++) {
+            if (this.desiredAlphaReducts[i] != null) {
+                for (int j = 0; j < this.desiredAlphaReducts[i].length; j++) {
+                    reductsList.add(this.desiredAlphaReducts[i][j]);
+                }
+            }
+        }
+        List<String>[] reducts = reductsList.toArray(new List[reductsList.size()]);
+
+        return ReductData.initialize(absorptionLaw(reducts));
+    }
+
+    public List<String>[] convertCNF2DNF(List<String>[] inputList) {
+        int numOfDNF = 1;
+        for (int i = 0; i < inputList.length; i++) {
+            numOfDNF *= inputList[i].size();
+        }
+        List<String>[] listDNF = new ArrayList[numOfDNF];
+
+        int[] position = new int[inputList.length];
+        for (int i = 0; i < listDNF.length; i++) {
+            for (int j = 0; j < inputList.length; j++) {
+                if (listDNF[i] == null) {
+                    listDNF[i] = new ArrayList();
+                }
+                if (position[j] >= inputList[j].size()) {
+                    position[j] = 0;
+                }
+                inputList[j].get(position[j]);
+                listDNF[i].add(inputList[j].get(position[j]));
+                position[j]++;
+            }
+        }
+        return listDNF;
+    }
+
+    public List<String>[] absorptionLaw(List<String>[] inputList) {
+        for (int i = 0; i < inputList.length - 1; i++) {
+            for (int j = i + 1; j < inputList.length; j++) {
+                if (inputList[i] != null && inputList[j] != null) {
+                    if (inputList[i].size() <= inputList[j].size()) {
+                        if (isImplicant(inputList[i], inputList[j])) {
+                            inputList[j] = null;
+                        }
+                    } else {
+                        if (isImplicant(inputList[j], inputList[i])) {
+                            inputList[i] = null;
+                        }
+                    }
+                }
+            }
+        }
+
+        List<List<String>> result = new ArrayList<List<String>>();
+        for (List<String> list : inputList) {
+            if (list != null) {
+                result.add(list);
+            }
+        }
+        return result.toArray(new List[result.size()]);
+    }
+
+    public boolean isImplicant(List<String> a, List<String> b) {
+        for (String strA : a) {
+            boolean isFound = false;
+            for (String strB : b) {
+                if (strA.equals(strB)) {
+                    isFound = true;
+                }
+            }
+            if (!isFound) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
---
+++ KDD/test/kdd/data/DiscernableDataTest.java
@@ -0,0 +1,248 @@
+package kdd.data;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kdd.ui.CSVFile;
+
+import org.junit.Test;
+
+public class DiscernableDataTest {
+
+    @Test
+    public void testIsImplicant1() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("c1");
+        a.add("e1");
+        List<String> b = new ArrayList<String>();
+        b.add("c1");
+        b.add("e1");
+        b.add("b2");
+
+        assertTrue(data.isImplicant(a, b));
+    }
+
+    @Test
+    public void testIsImplicant2() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("c1");
+        a.add("b1");
+        List<String> b = new ArrayList<String>();
+        b.add("c1");
+        b.add("e1");
+        b.add("b2");
+
+        assertFalse(data.isImplicant(a, b));
+    }
+
+    @Test
+    public void testIsImplicant3() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("c1");
+        a.add("e1");
+        a.add("b2");
+        a.add("e2");
+        List<String> b = new ArrayList<String>();
+        b.add("c1");
+        b.add("e1");
+        b.add("b2");
+
+        assertFalse(data.isImplicant(a, b));
+    }
+
+    @Test
+    public void testIsImplicant4() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("c1");
+        List<String> b = new ArrayList<String>();
+        b.add("c1");
+        b.add("e1");
+        b.add("b2");
+
+        assertTrue(data.isImplicant(a, b));
+    }
+
+    @Test
+    public void testIsImplicant5() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("c1");
+        List<String> b = new ArrayList<String>();
+        b.add("c1");
+
+        assertTrue(data.isImplicant(a, b));
+    }
+
+    @Test
+    public void testAbsorptionLaw1() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("b2");
+        List<String> b = new ArrayList<String>();
+        b.add("b2");
+        List<String> c = new ArrayList<String>();
+        c.add("c1");
+        c.add("e1");
+        List<String> d = new ArrayList<String>();
+        d.add("b2");
+        d.add("c1");
+        d.add("e1");
+
+        List<String>[] result = data.absorptionLaw(new List[] { a, b, c, d });
+
+        assertEquals(2, result.length);
+        assertEquals(1, result[0].size());
+        assertEquals("b2", result[0].get(0));
+        assertEquals(2, result[1].size());
+        assertEquals("c1", result[1].get(0));
+        assertEquals("e1", result[1].get(1));
+    }
+
+    @Test
+    public void testAbsorptionLaw2() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("b1");
+        List<String> b = new ArrayList<String>();
+        b.add("b2");
+        List<String> c = new ArrayList<String>();
+        c.add("c1");
+        c.add("e1");
+        List<String> d = new ArrayList<String>();
+        d.add("b2");
+        d.add("c1");
+        d.add("e1");
+
+        List<String>[] result = data.absorptionLaw(new List[] { a, b, c, d });
+
+        assertEquals(3, result.length);
+        assertEquals(1, result[0].size());
+        assertEquals("b1", result[0].get(0));
+        assertEquals(1, result[1].size());
+        assertEquals("b2", result[1].get(0));
+        assertEquals(2, result[2].size());
+        assertEquals("c1", result[2].get(0));
+        assertEquals("e1", result[2].get(1));
+    }
+
+    @Test
+    public void testAbsorptionLaw3() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("b1");
+        a.add("b2");
+        List<String> b = new ArrayList<String>();
+        b.add("b2");
+        List<String> c = new ArrayList<String>();
+        c.add("c1");
+        c.add("e1");
+        List<String> d = new ArrayList<String>();
+        d.add("b2");
+        d.add("c1");
+        d.add("e1");
+
+        List<String>[] result = data.absorptionLaw(new List[] { a, b, c, d });
+
+        assertEquals(2, result.length);
+        assertEquals(1, result[0].size());
+        assertEquals("b2", result[0].get(0));
+        assertEquals(2, result[1].size());
+        assertEquals("c1", result[1].get(0));
+        assertEquals("e1", result[1].get(1));
+    }
+
+    @Test
+    public void testConvertCNF2DNF1() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("b2");
+        List<String> b = new ArrayList<String>();
+        b.add("c1");
+        b.add("e1");
+
+        List<String>[] result = data.convertCNF2DNF(new List[] { a, b });
+        assertEquals(2, result.length);
+        assertEquals(2, result[0].size());
+        assertEquals("b2", result[0].get(0));
+        assertEquals("c1", result[0].get(1));
+        assertEquals(2, result[1].size());
+        assertEquals("b2", result[1].get(0));
+        assertEquals("e1", result[1].get(1));
+    }
+
+    @Test
+    public void testConvertCNF2DNF2() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("b1");
+        a.add("b2");
+        List<String> b = new ArrayList<String>();
+        b.add("c1");
+        b.add("e1");
+
+        List<String>[] result = data.convertCNF2DNF(new List[] { a, b });
+        assertEquals(4, result.length);
+        assertEquals(2, result[0].size());
+        assertEquals("b1", result[0].get(0));
+        assertEquals("c1", result[0].get(1));
+        assertEquals(2, result[1].size());
+        assertEquals("b2", result[1].get(0));
+        assertEquals("e1", result[1].get(1));
+
+        assertEquals(2, result[2].size());
+        assertEquals("b1", result[2].get(0));
+        assertEquals("c1", result[2].get(1));
+        assertEquals(2, result[3].size());
+        assertEquals("b2", result[3].get(0));
+        assertEquals("e1", result[3].get(1));
+    }
+
+    @Test
+    public void testConvertCNF2DNF3() {
+        DiscernableData data = DiscernableData.initialize(null, null, null);
+        List<String> a = new ArrayList<String>();
+        a.add("b1");
+        a.add("b2");
+        List<String> b = new ArrayList<String>();
+        b.add("c1");
+        b.add("e1");
+        b.add("f1");
+
+        List<String>[] result = data.convertCNF2DNF(new List[] { a, b });
+        assertEquals(6, result.length);
+        assertEquals(2, result[0].size());
+        assertEquals("b1", result[0].get(0));
+        assertEquals("c1", result[0].get(1));
+        assertEquals(2, result[1].size());
+        assertEquals("b2", result[1].get(0));
+        assertEquals("e1", result[1].get(1));
+        assertEquals(2, result[2].size());
+        assertEquals("b1", result[2].get(0));
+        assertEquals("f1", result[2].get(1));
+
+        assertEquals(2, result[3].size());
+        assertEquals("b2", result[3].get(0));
+        assertEquals("c1", result[3].get(1));
+        assertEquals(2, result[4].size());
+        assertEquals("b1", result[4].get(0));
+        assertEquals("e1", result[4].get(1));
+        assertEquals(2, result[5].size());
+        assertEquals("b2", result[5].get(0));
+        assertEquals("f1", result[5].get(1));
+    }
+
+    @Test
+    public void testCalculateReductData1() throws Exception {
+        OriginalData originalData = CSVFile.readCSV("test/dataSet1.csv");
+        DiscernableData discernableData = originalData.caLculateDiscernableData();
+
+        ReductData result = discernableData.calculateReductData();
+        assertEquals(3, 1);
+    }
+}
